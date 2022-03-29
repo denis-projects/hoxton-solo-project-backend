750b1ce8268e36db16d66e05c971431997bbda9e
besmaerteralb <denis.mimini.1@outlook.com>
2022-03-29 12:20:40 +0200

    setting things up

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ import helmet from "helmet";
 import morgan from "morgan";
 
 const app = express();
+
+// making a secure connection 
 dotenv.config()
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, () => {
+    console.log(`Connected`);
+});
+
+
+// set the env to use the dependencies we installed
+
+app.use(express())
+app.use(helmet())
+app.use(morgan("common"))
+
 
 app.listen(6666, () => {
     console.log(`Server is running`);
