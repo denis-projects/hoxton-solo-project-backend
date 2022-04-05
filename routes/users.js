9dce365ddbd4b5ccba1e1563cd4e78b2a4e688ad
besmaerteralb <denis.mimini.1@outlook.com>
2022-04-05 12:51:42 +0200

    udating user

--- routes/users.js
+++ routes/users.js
@@ -12,29 +12,27 @@ router.get("/", (req, res) => {
 // Update a user
 
 router.put("/:id", async (req, res) => {
-
     if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
-                const salt = await bcrypt.genSalt(4)
-                req.body.password = await bcrypt.hash(req.body.password, salt)
+                const salt = await bcrypt.genSalt(4);
+                req.body.password = await bcrypt.hash(req.body.password, salt);
             } catch (err) {
-                return res.status(500).send(err)
+                return res.status(500).json(err);
             }
         }
         try {
             const user = await User.findByIdAndUpdate(req.params.id, {
-                $set: req.body
-            })
-            res.status(200).send("Your account has been updated")
+                $set: req.body,
+            });
+            res.status(200).json("Account has been updated");
         } catch (err) {
-            res.status(500).send(err)
+            return res.status(500).json(err);
         }
-
     } else {
-        return res.status(403).send("Access denied. You can update only your account")
+        return res.status(403).json("Access Denied!");
     }
-})
+});
 
 // Delete a user
 
